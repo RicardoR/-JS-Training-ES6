10b6a2b828ebbc8c542fe22bf2a695bc18f0476c
Ricardo <ricardo.rv@outlook.com>
2016-08-07 13:22:05 +0200

    Primer ejercicio de uso de every

--- every_some/script.js
+++ every_some/script.js
@@ -1,3 +1,14 @@
-/* TO DO */
-/* */
+/* Finding Submitted Users */
+/* Given an array of users, return 'true' if every user has submitted a request form.  Assign the result to the variable 'hasSumbmitted' */
 
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted;
+
+hasSubmitted = users.every(function(user){
+	return user.hasSubmitted;
+});
