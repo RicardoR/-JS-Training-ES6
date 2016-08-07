4e269eaae0095e9a02d130e5610e26e818678e8a
Ricardo <ricardo.rv@outlook.com>
2016-08-07 13:24:51 +0200

    Ejercicio de some

--- every_some/script.js
+++ every_some/script.js
@@ -12,3 +12,19 @@ var hasSubmitted;
 hasSubmitted = users.every(function(user){
 	return user.hasSubmitted;
 });
+
+
+/* In Progress Network Requests */
+/*Given an array of network objects representing network requests, assign the boolean 'true' to the variable 'inProgress' if any network request has a 'status' of 'pending'.*/
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress;
+
+inProgress = requests.some(function(request){
+	return request.status === 'pending';
+});
