c67b9e0e7b74e6093a8293c24dcaa92be8e869bf
Ricardo <ricardo.rv@outlook.com>
2016-08-14 09:33:05 +0200

    Ejercicios const y let completados

--- const_let/script.js
+++ const_let/script.js
@@ -6,3 +6,23 @@ Declare three variables with these same names, making sure to use 'const' or 'le
 const name = 'Ricardo';
 const dateOfBirth = '21/04/1981';
 let age = 35;
+
+
+/*Const/Let Refactoring */
+
+/*The following code uses 'var' instead of 'const' and 'let'. Refactor the function to use the new keywords.  Be sure to consider whether the variable should be declared using 'const' or 'let' depending on whether the variable gets reassigned or not.*/
+
+const statuses = [ 
+  { code: 'OK', response: 'Request successful' },
+  { code: 'FAILED', response: 'There was an error with your request' },
+  { code: 'PENDING', response: 'Your reqeust is still pending' }
+];
+let message = '';
+let currentCode = 'OK';
+
+for (var i = 0; i < statuses.length; i++) {
+  if (statuses[i].code === currentCode) {
+    message = statuses[i].response;
+  }
+}
+
