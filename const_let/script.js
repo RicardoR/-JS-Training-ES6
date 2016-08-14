3dcc2c79056922e7e4f3d110064ee539196447ca
Ricardo <ricardo.rv@outlook.com>
2016-08-14 09:30:44 +0200

    Ejercicio de const y let

---
+++ const_let/script.js
@@ -0,0 +1,8 @@
+/*A Constant Exercise of Letting Variables Be Variables*/
+
+/*Imagine that you are building an application to manage a user's Facebook profile.  A profile might have a 'name', 'age', and 'dateOfBirth'.
+Declare three variables with these same names, making sure to use 'const' or 'let' depending on whether you expect the value to change over time.*/
+
+const name = 'Ricardo';
+const dateOfBirth = '21/04/1981';
+let age = 35;
