072f136c89a2be0a436f52c0b4328dda67e70ffb
RicardoR <ricardo.rv@outlook.com>
2016-08-20 13:41:21 +0200

    Segundo ejercicio de refactorización de objetos resuelto

--- L11_Enhanced_Object_Literals/script.js
+++ L11_Enhanced_Object_Literals/script.js
@@ -6,3 +6,12 @@ const red = '#ff0000';
 const blue = '#0000ff';
 
 const COLORS = { red, blue };
+
+
+/*Condensing Code with Enhanced Literals*/
+
+/*Refactor the following to use enhance object literal syntax*/
+
+const fields = ['firstName', 'lastName', 'phoneNumber'];
+
+const props = { fields };
