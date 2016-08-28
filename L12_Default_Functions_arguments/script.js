1ffc888b059d1d1489b8cde1d8c07081c1a375d5
Ricardo <ricardo.rv@outlook.com>
2016-08-28 13:35:43 +0200

    Argumentos opcionales en funciones

--- L12_Default_Functions_arguments/script.js
+++ L12_Default_Functions_arguments/script.js
@@ -7,3 +7,14 @@ function sum(a = 0, b = 0) {
 }
 
 sum();
+
+
+/*Dumping Unused Code*/
+
+/*Refactor the following code to use default function arguments.  Be sure to remove any unused code after you refactor it.*/
+function addOffset(style = {}) {
+  style.offset = '10px';
+  return style;
+}
+
+addOffset();
