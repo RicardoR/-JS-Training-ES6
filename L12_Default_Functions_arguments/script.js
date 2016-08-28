eb1af2b79443a2345fe8463e37d687506ddb6b0c
Ricardo <ricardo.rv@outlook.com>
2016-08-28 13:32:09 +0200

    Argumentos opcionales en funciones

---
+++ L12_Default_Functions_arguments/script.js
@@ -0,0 +1,9 @@
+/*Using Default Arguments*/
+
+/*Refactor the following code to use default function arguments.  Be sure to remove any unused code after you refactor it.*/
+
+function sum(a = 0, b = 0) {
+  return a + b;
+}
+
+sum();
