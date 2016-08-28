841c60c4b433547cf57ee24a55f99f1279374488
Ricardo <ricardo.rv@outlook.com>
2016-08-28 13:58:33 +0200

    Ejercicios rest / spreat

---
+++ L13_Rest_And_Spread_Operator/script.js
@@ -0,0 +1,24 @@
+/*Many, Many Arguments*/
+
+/*Refactor the following function to use the rest operator.  Remember, an argument using the rest operator does *not* need to be called 'rest'.*/
+
+function product(...numbers) {
+  return numbers.reduce(function(acc, number) {
+    return acc * number;
+  }, 1)
+}
+
+product(1,2,3,4);
+
+
+/*Spreadin' Arrays*/
+
+/*Refactor the following to use the spread operator.*/
+
+function join(array1, array2) {
+  return [...array1, ...array2];
+}
+
+var array1 = ['a', 'b', 'c'];
+var array2 = ['1', '2', '3'];
+join(array1, array2)
