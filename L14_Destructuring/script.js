3702656f8986c24970fab5f6fb177bb7d80ce979
Ricardo <ricardo.rv@outlook.com>
2016-09-08 11:05:56 +0200

    Tercer ejercicio de destructuring

--- L14_Destructuring/script.js
+++ L14_Destructuring/script.js
@@ -37,3 +37,29 @@ const classesAsObject = classes.map(([subject, time, teacher]) => {
 });
 
 classesAsObject;
+
+
+/*Recursion with Destructuring*/
+
+/*This one is probably the hardest exercise in the entire course!  
+
+Use array destructuring, recursion, and the rest/spread operators to create a function 'double' that will return a new array with all values inside of it multiplied by two.  Do not use any array helpers! Sure, the map, forEach, or reduce helpers would make this extremely easy but give it a shot the hard way anyways :)
+
+Input:
+
+double([1,2,3])
+
+Output
+
+[2,4,6]
+
+Hint: Don't forget that with recursion you must add a base case so you don't get an infinite call stack.  For example, if 'const [ number, ...rest ] = numbers' and number is undefined do you need to keep walking through the array?*/
+
+function double(numbers) {
+    const [b,...rest] = numbers
+    return (rest.length ? [b*2, ...double(rest)] : [b*2])
+}
+
+const numbers = [1, 2, 3];
+
+double(numbers);
