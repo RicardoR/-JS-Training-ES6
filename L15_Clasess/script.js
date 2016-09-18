acf74a829101ffd196dd0b477d606957d065ce39
RicardoR <ricardo.rv@outlook.com>
2016-09-18 18:50:21 +0200

    Segundo ejercicio de clases

--- L15_Clasess/script.js
+++ L15_Clasess/script.js
@@ -1,17 +1,48 @@
-/*Game Classes*/
+/* Game Classes */
 
 /*
 You are a game developer tasked with setting up some basic classes for a new game you are working on.  Create a class called 'Monster'.  In the constructor, you'll need to do some basic setup for Monster whenever they are created.
 
   * Initialize the Monster's health to 100.
-  * The constructor will be called with an 'options' object that has a 'name' property.  Assign the 'name' to the Monster */
+  * The constructor will be called with an 'options' object that has a 'name' property.
+    Assign the 'name' to the Monster
+*/
 
 
-  class Monster {
-    constructor(options) {
-    	this.health = 100;
-      this.name = options.name;
-    }
+class Monster {
+  constructor(options) {
+  	this.health = 100;
+    this.name = options.name;
   }
+}
 
-  const monster = new Monster({name:"Lestrigones"});
+const monster = new Monster({name:"Lestrigones"});
+
+
+/* Subclassing Monsters */
+
+/*
+Now that you have a monster created, create a subclass of the Monster called Snake.
+
+The Snake should have a 'bite' method.  The only argument to this method is another instance of a Snake.
+The instance of Snake that is passed in should have their health deducated by 10
+*/
+
+class Monster {
+  constructor(options) {
+    this.health = 100;
+    this.name = options.name;
+  }
+}
+
+class Snake extends Monster{
+	bite(snake) {
+  	snake.health -= 10;
+  }
+}
+
+const snake = new Snake({name: "Cobra"});
+const snake2 = new Snake({name: "Cascabel"});
+
+snake.bite(snake2);
+console.log(snake2);
