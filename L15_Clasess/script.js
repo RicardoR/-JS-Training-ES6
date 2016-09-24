6391f1c41164c8c50b5485dcd14176071a837907
Ricardo <ricardo.rv@outlook.com>
2016-09-24 12:34:21 +0200

    Ejercicios de clases terminados

--- L15_Clasess/script.js
+++ L15_Clasess/script.js
@@ -15,3 +15,32 @@ You are a game developer tasked with setting up some basic classes for a new gam
   }
 
   const monster = new Monster({name:"Lestrigones"});
+
+
+/*Subclassing Monsters*/
+
+/*
+Now that you have a monster created, create a subclass of the Monster called Snake.  
+
+The Snake should have a 'bite' method.  The only argument to this method is another instance of a Snake.
+The instance of Snake that is passed in should have their health deducated by 10
+*/
+
+class Monster {
+  constructor(options) {
+    this.health = 100;
+    this.name = options.name;
+  }
+}
+
+class Snake extends Monster{
+	bite(snake) {
+  	snake.health -= 10;
+  }
+}
+
+const snake = new Snake({name: "Cobra"});
+const snake2 = new Snake({name: "Cascabel"});
+
+snake.bite(snake2);
+snake2;
