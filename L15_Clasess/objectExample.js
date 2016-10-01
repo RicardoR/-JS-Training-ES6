a6593617ba51b8bd8de4deef3b40ed1dd2df0f2a
RicardoR <ricardo.rv@outlook.com>
2016-10-01 16:42:02 +0200

    Ejemplo de objetos

---
+++ L15_Clasess/objectExample.js
@@ -0,0 +1,20 @@
+function createBookShop(inventory) {
+  return {
+    inventory: inventory,
+    inventoryValue: function() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceForTitle: function(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  };
+}
+
+const inventory = [
+  { title: 'Harry Potter', price: 10 },
+  { title: 'Eloquent JavasCript', price: 15}
+]
+
+const bookShop = createBookShop(inventory);
+console.log(bookShop);
+console.log(bookShop.inventoryValue());
