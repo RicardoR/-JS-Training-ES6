eb586a73b6301bb8b60e5adcbf6b4e19253eaf89
RicardoR <ricardo.rv@outlook.com>
2016-10-16 21:40:40 +0200

    Primera lección del curso zsolt, arrow function

---
+++ Zsolt_Nagy_Course/Lesson_01_Arrow_Functions/script.js
@@ -0,0 +1,50 @@
+//Exercise 1: Write an arrow function that returns the string 'Hello World!'.
+
+var salutation = () => console.log("Hello World!");
+salutation();
+
+//Exercise 2: Write an arrow function that expects an array of integers, and returns the sum of the elements of the array.
+//Use the built-in method reduce on the array argument.
+
+var numberArray = [1,4,5,8,9,12];
+var arraySum = (numberArray) => {
+    return numberArray.reduce((prev, curr) => prev + curr);
+};
+var sum = arraySum(numberArray);
+console.log(sum);
+
+//Exercise 3: Rewrite the following code by using arrow functions wherever it makes sense to use them:
+
+/*var Entity = function( name, delay ) {
+this.name = name;
+this.delay = delay;
+};
+
+Entity.prototype.greet = function() {
+    setTimeout( function() {
+        console.log( 'Hi, I am ' + this.name );
+    }.bind( this ), this.delay );
+};
+
+var java = new Entity( 'Java', 5000 );
+var cpp = new Entity( 'C++', 30 );
+
+java.greet();
+cpp.greet(); */
+
+var Entity = function( name, delay ) {
+    this.name = name;
+    this.delay = delay;
+};
+
+Entity.prototype.greet = function() {
+    setTimeout( () => {
+        console.log( 'Hi, I am ' + this.name );
+    }, this.delay );
+};
+
+var java = new Entity( 'Java', 5000 );
+var cpp = new Entity( 'C++', 30 );
+
+java.greet();
+cpp.greet();
