7990957a75c85d51240f9439f964d5e09654fbc4
RicardoR <ricardo.rv@outlook.com>
2016-10-24 23:19:26 +0200

    Ejercicios let y var

---
+++ Zsolt_Nagy_Course/Lesson_02_Function scope_block_scope_constants/script.js
@@ -0,0 +1,63 @@
+/*
+Modify the code such that all six console logs print out their values exactly once, and the printed values are the following:
+1 3
+1 3
+1 2
+5
+5 6
+1 2
+*/
+/*
+*
+ 'use strict';
+
+ var guessMe1 = 1;
+ let guessMe2 = 2;
+
+ {
+ try {
+ console.log( guessMe1, guessMe2 );
+ } catch( _ ) {}
+
+ let guessMe2 = 3;
+ console.log( guessMe1, guessMe2 );
+ }
+
+ console.log( guessMe1, guessMe2 );
+
+ () => {
+
+ console.log( guessMe1 );
+ var guessMe1 = 5;
+ let guessMe2 = 6;
+ console.log( guessMe1, guessMe2 );
+ };
+
+ console.log( guessMe1, guessMe2 );
+* */
+'use strict';
+
+var guessMe1 = 1;
+let guessMe2 = 2;
+
+{
+    try {
+        let guessMe2 = 3;
+        console.log( guessMe1, guessMe2 );
+    } catch( _ ) {}
+
+    let guessMe2 = 3;
+    console.log( guessMe1, guessMe2 );
+}
+
+console.log( guessMe1, guessMe2 );
+
+var displayGuess = () => {
+    var guessMe1 = 5;
+    console.log( guessMe1 );
+
+    let guessMe2 = 6;
+    console.log( guessMe1, guessMe2 );
+};
+displayGuess();
+console.log( guessMe1, guessMe2 );
