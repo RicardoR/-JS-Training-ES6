8003b805c74c7ed3d4cdeb410a684b5702f5506e
RicardoR <rirova81@gmail.com>
2017-01-17 20:01:49 +0100

    Termino ejercicios de destructuring. Cambio estilos en salida de código

--- Acamica/es6-tutorial/src/app.js
+++ Acamica/es6-tutorial/src/app.js
@@ -1,1 +1,15 @@
 const sum = (a, b) => a+b;
+
+
+// 2.1. Let & const
+const PI = 3.141593;
+
+let calcularArea = function(radio){
+    let area = ( radio * 2 ); 
+    area = area * PI;
+    return area;
+}; 
+
+console.log('El valor de PI es ' + PI +
+' y de un circulo con radio ' +  3 + 
+' el valor del area es: '+calcularArea(3) );
